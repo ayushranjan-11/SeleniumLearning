1b1c405d60b9c72a4c3f1ca55ff07069111a6989
ayush <ayush.ranjan921@gmail.com>
2023-08-20 01:18:37 +0530

    Added code to select options from passenger dropdown and increase the value with button press

--- src/main/java/org/example/RahulShettyCourse/StaticDropdown.java
+++ src/main/java/org/example/RahulShettyCourse/StaticDropdown.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class StaticDropdown {
@@ -13,7 +15,8 @@ public class StaticDropdown {
         WebDriver webDriver = new ChromeDriver();
         String websiteUrl = "https://rahulshettyacademy.com/dropdownsPractise/";
         browserSetup(webDriver, websiteUrl);
-        staticDropdownOperations(webDriver);
+//      staticDropdownOperations(webDriver);
+        passengersDropdownOption(webDriver);
     }
     public static void browserSetup(WebDriver webDriver, String websiteUrl) {
         webDriver.get(websiteUrl);
@@ -27,18 +30,67 @@ public class StaticDropdown {
         Select select = new Select(currencyDropDownOption);
         List<WebElement>availableOptions = select.getOptions();
 
-        //Display all available options in the static class
+        //Prints all available options in the static class
         for (int i = 0; i<availableOptions.size();i++ ) {
             System.out.println(availableOptions.get(i).getText());
         }
 
         //To select all options from start to end, one by one
+//        if (!availableOptions.isEmpty()) {
+//            for (int j =0; j< availableOptions.size(); j++) {
+//                select.selectByIndex(j);
+//                Thread.sleep(1000);
+//            }
+//
+//        }
+
+
+        // Selecting option with visible text command
         if (!availableOptions.isEmpty()) {
-            for (int j =0; j< availableOptions.size(); j++) {
-                select.selectByIndex(j);
-                Thread.sleep(1000);
-            }
+            select.selectByVisibleText("INR");
+            System.out.println("Now you have selected: "
+                    +select.getFirstSelectedOption().getText());
+        } else System.out.println("Unable to grab options from the dropdown");
+    }
+
+    public static void passengersDropdownOption(WebDriver webDriver) throws InterruptedException {
+        By passengerDropdownOption = By.xpath("//div[@id='divpaxinfo']");
+
+        //Adult option in the dropdown
+        By adultOptionIncreaseButton = By.xpath("//span[@id='hrefIncAdt']");
+        By adultOptionDecreaseButton = By.xpath("//span[@id='hrefDecAdt']"); //Decrease button is only available when number of adult is available more than 1
+
+        //Child option in the dropdown
+        By childOptionIncreaseButton = By.xpath("//span[@id='hrefIncChd']");
+        By childOptionDecreaseButton = By.xpath("//span[@id='hrefDecChd']"); //Decrease button is only available when number of adult is available more than 1
+
+        //Infant option in the dropdown
+        By infantOptionIncreaseButton = By.xpath("//span[@id='hrefIncInf']");
+        By infantOptionDecreaseButton = By.xpath("//span[@id='hrefDecInf']"); //Decrease button is only available when number of adult is available more than 1
+
+        //Done button
+        By doneButtonForDropdown = By.xpath("//input[@id='btnclosepaxoption']");
+
+        //Opening passenger drop don and selecting add from all options
+        webDriver.findElement(passengerDropdownOption).click();
+
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        try {
+
+            Thread.sleep(1000);
+            webDriver.findElement(adultOptionIncreaseButton).click();
+            webDriver.findElement(childOptionIncreaseButton).click();
+            webDriver.findElement(infantOptionIncreaseButton).click();
+
+        } catch (Exception e) {
 
         }
+
+        //Clicking done button after operation
+        webDriver.findElement(doneButtonForDropdown).click();
+
+        //Getting text of total number of selected options in the passenger dropdown
+        System.out.println(webDriver.findElement(passengerDropdownOption).getText());
+
     }
 }
